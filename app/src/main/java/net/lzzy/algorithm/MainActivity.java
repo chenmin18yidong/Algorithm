ab01ce9c3e88cb108c6630d840d853a9c898405a
Liangsimin <320366897@qq.com>
2019-06-08 16:29:51 +0800

    选择排序1

--- app/src/main/java/net/lzzy/algorithm/MainActivity.java
+++ app/src/main/java/net/lzzy/algorithm/MainActivity.java
@@ -56,32 +56,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     }
     private void directSort() {
         //todo:直接选择排序的具体实现
-        //分为有序区和无序区，每一趟排序都在无序区依次对比，记录对比区域的最小元素的位置
-        //然后把无序区第一个元素的最小元素进行交换，无序区少=一个，有序区多一个，循环往复至无序区
-        //for(int i=0;i<items.length-1;i++){
-        //int min=i;
-        //for(int j=i+1;j<items.length;j++){
-      //if(itms[min].coparTO(items[j])>0){
-        //min=j;
-        //}}
-        //swap(min,i)
 
-        for(int i=0; i<items.length; i++) {
-            for(int j=i+1; j<items.length; j++) {
-                if(items[i] < items[j]) {
-                    int temp = items[i];
-                    items[i] = items[j];
-                    items[j] = temp;
-                }
-            }
-        }
-    }
-
-    private void generateItems() {
-        items = new Integer[10];
-        Random generator = new Random();
-        for (int i = 0; i < items.length; i++) {
-            items[i] = generator.nextInt(99);
-        }
     }
 }
